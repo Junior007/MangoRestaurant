e9e9022949b30ac22ce9d8dab59612561c053e28
Junior007 <agilgomez@gmail.com>
2021-10-16 11:58:38 +0200

    no le gusta el formato program.cs sin startup, subo para guardar una referencia en git y lo cambio

--- mango.identity/Program.cs
+++ mango.identity/Program.cs
@@ -1,7 +1,40 @@
+using mango.identity;
+using mango.identity.DBContext;
+using mango.identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//IdentityContext (data base)
+builder.Services.AddDbContext<ApplicationDBContext>(c =>
+    c.UseSqlServer(builder.Configuration["ConnectionStrings:MangoIdentityDB"], b => b.MigrationsAssembly("mango.identity")));// b => b.MigrationsAssembly("mango.product.api")) ; , ServiceLifetime.Singleton);
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDBContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(SD.IdentityResources)
+.AddInMemoryApiScopes(SD.ApiScopes)
+.AddInMemoryClients(SD.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+/*builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddScoped<IProfileService, ProfileService>();*/
+//builder.Services.AddDeveloperSigningCredential();
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
@@ -17,7 +50,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
